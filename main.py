cba1f9b56267585812b7877a50b6aaa344025dbf
ac-optimus <abhavya.chandra@iitgn.ac.in>
2019-11-23 04:12:10 +0530

    adding final project

---
+++ main.py
@@ -0,0 +1,100 @@
+from tkinter import *
+from tkinter import messagebox
+from model import ONE_COMPARTMENT, TWO_COMPARTMENT
+import json
+import numpy as np
+
+PATH_JSON = "constants.json"
+print ("reading values from -",PATH_JSON)
+with open(PATH_JSON) as f:
+    args = json.load(f)
+   
+
+def plot_concentration(FLAG = "ONE COMPARTMENT"):
+    t = np.linspace(0,24,1000)
+    #parameters
+    if FLAG[:3] == "ONE" :
+
+        F = args['F']
+        K_a = args['K_a']
+        K = args['K']
+        V = args['V1']
+        A_O = args["INP_A"]
+        tau = args["tau"]
+        max_dose = args["max_dose"]
+
+        ONE = ONE_COMPARTMENT(F, K_a, K, V, A_O, tau, max_dose)
+        ONE.plot(FLAG,t)
+
+    elif FLAG[:3] == "TWO":
+
+        k21 = args["k21"]
+        k12 = args["k12"]
+        K_a = args["K_a"]
+        K = args["K"]
+        F = args["F"] 
+        A_gut = args["INP_A"]
+        V1 = args["V1"]
+        V2 = args["V2"]
+        A_initial  = [args["A1"], args["A2"]]
+        TWO = TWO_COMPARTMENT(k21, k12, K_a, K, F, A_gut, V1, V2, A_initial, 4)
+        TWO.plot(FLAG, t)
+    return 0 
+
+def close():
+    sys.exit()
+    # global root
+    # root.quit()
+    return
+
+def sel():
+   txt = var.get()
+   if txt == 0:
+        model = "ONE COMPARTMENT-Intravenous Bolus( SINGLE DOSE )"
+   if txt == 1:
+        model = "ONE COMPARTMENT-Intravenous Bolus( CONSTANT RATE INFLUX)"
+   elif txt == 2:
+       model = "ONE COMPARTMENT-ORAL( SINGLE DOSE )"
+   elif txt == 3:
+        model = "ONE COMPARTMENT-ORAL( MULTIPLE DOSE )"
+   elif txt == 4:
+       model = "TWO COMPARTMENT-Intravenous Bolus( SINGLE DOSE )"
+   elif txt == 5:
+        model = "TWO COMPARTMENT-Intravenous Bolus( CONSTANT RATE INFLUX)"
+   elif txt == 6:
+        model = "TWO COMPARTMENT-ORAL( SINGLE DOSE )"
+   elif txt == 7:
+        model = "TWO COMPARTMENT-ORAL( MULTIPLE DOSE )"
+
+   messagebox.showinfo(model, "detials of the compartment here. Press OK to see the plots!")
+
+   plot_concentration(model)
+
+root = Tk() 
+root.geometry("1024x240+500+360") 
+var = IntVar()
+root.title("SELECT THE COMPARMENT MODEL")
+models = [\
+    "ONE COMPARTMENT-Intravenous Bolus( SINGLE DOSE )",\
+    "ONE COMPARTMENT-Intravenous Bolus( CONSTANT RATE INFLUX)",\
+    "ONE COMPARTMENT-ORAL( SINGLE DOSE )",\
+    "ONE COMPARTMENT-ORAL( MULTIPLE DOSE )",\
+    "TWO COMPARTMENT-Intravenous Bolus( SINGLE DOSE )",\
+    "TWO COMPARTMENT-Intravenous Bolus( CONSTANT RATE INFLUX)", \
+    "TWO COMPARTMENT-ORAL( SINGLE DOSE )",\
+    "TWO COMPARTMENT-ORAL( MULTIPLE DOSE )", \
+    "QUIT"]
+
+for i in range(len(models)):
+    button = Radiobutton(root, text = models[i], variable = var,  
+                value = i, indicator = 0, 
+                background = "light blue", command=sel)
+    if models[i] == "QUIT":
+        button = Radiobutton(root, text = models[i], variable = var,  
+                    value = i, indicator = 0, 
+                    background = "light blue", command=close)        
+    button.pack(fill = X, ipady = 5)
+
+label = Label(root)
+label.pack()
+root.mainloop()
---
+++ model.py
@@ -0,0 +1,183 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+plt.style.use('seaborn-bright')
+
+class ONE_COMPARTMENT:
+    """single compartment model for PK Modelling"""
+    def __init__(self, F, K_a, K, V, A_O, tau, max_dose):
+        self.F = F
+        self.K_a = K_a
+        # self.A_gut = A_gut
+        self.K = K
+        self.V = V
+        self.A_O = A_O
+        self.tau = tau
+        self.max_dose = max_dose
+
+    def A_ORAL(self, t):
+        fun = self.K_a*self.F*self.A_O\
+            /(self.K_a - self.K)*\
+            (np.exp(-self.K*t) - np.exp(-self.K_a*t))
+        return fun
+
+    def A_bolus(self, t):
+        A_Bolus =  self.A_O*np.exp(-self.K*t)
+        return A_Bolus
+
+    def C_oral(self, t):
+        C_Oral = self.A_ORAL(t)/self.V 
+        return C_Oral
+
+    def C_bolus(self, t):
+        C_Bolus = self.A_bolus(t)/self.V
+        return C_Bolus
+
+    def md_unit(self, n, t):
+        k = self.K
+        k_a = self.K_a
+        tau  =self.tau
+        f = 25*(np.exp(-k*(t-n*tau)) - np.exp(-k_a*(t-n*tau)))
+        return f
+
+    def val_t(self, t, tau, k, max_dose):
+        max_term = int(t/tau) if int(t/tau) < max_dose else max_dose 
+        cumm = self.md_unit(0,t)
+        for i in range(1, max_term+1):
+            cumm+=self.md_unit(i,t)
+        return cumm
+
+    def const_influx(self,t):
+        F = self.F
+        A_O = self.A_O
+        K = self.K
+        V1 = self.V
+        tau = self.tau
+        y = (F*A_O*(1-np.exp(-K*t)))/(tau*K*V1)
+        return y
+
+    def plot(self, flag, t):
+        if flag == "ONE COMPARTMENT-Intravenous Bolus( SINGLE DOSE )":
+            C = self.C_bolus(t)
+            plt.plot(t, C)
+  
+        elif flag == "ONE COMPARTMENT-Intravenous Bolus( CONSTANT RATE INFLUX)":
+           C = self.const_influx(t)
+           plt.plot(t, C)
+        elif flag == "ONE COMPARTMENT-ORAL( SINGLE DOSE )":
+            C = self.C_oral(t)
+            plt.plot(t, C)
+        elif flag == "ONE COMPARTMENT-ORAL( MULTIPLE DOSE )":
+            C = [0]*len(t)
+            for j in range(t.shape[0]):
+                C[j] = self.val_t(t[j], self.tau, self.K, self.max_dose )
+            plt.plot(t, C)
+        
+        plt.title(flag, size = 9)
+        plt.xlabel("Hours", size = 9)
+        plt.ylabel("Concentration(mg/L)", size=9)
+        plt.show()
+
+
+
+class TWO_COMPARTMENT:
+
+        def __init__(self, k21, k12, K_a, K, F, A_gut, V1, V2, A_initial, tau ):
+            self.k12 = k12
+            self.k21 = k21
+            self.k_a = K_a 
+            self.K = K
+            self.F  = F
+            self.A_gut = A_gut
+            self.V1 = V1
+            self.V2 = V2
+            self.A1 = A_initial[0]
+            self.A2 = A_initial[1]
+            self.tau = tau
+            self.max_dose = 3
+
+        def solver(self, interval, A):
+            A1, A2 = A[0], A[1]
+            eq1 = -self.k12*A1 + self.k21*A2- self.K*A1
+            eq2 = self.k12*A1 - self.k21*A2
+            return [eq1, eq2]
+        
+        def oral_singleDose(self,interval, A):
+            A1, A2, A_gut = A[0], A[1], A[2]
+            eq1 = self.F*self.k_a*self.A_gut + self.k21*A2 - self.k12*A1 - self.K*A1
+            eq2 = self.k12*A1 - self.k21*A2
+            eq3 = -self.k_a*self.A_gut
+            return [eq1, eq2, eq3]
+
+        def solver_multiple(self, interval, A):
+            A1, A2 = A[0], A[1]
+            eq1 = -self.k12*A1 + self.k21*A2 + self.F*self.k_a*self.A_gut - self.K*A1
+            eq2 = self.k12*A1 - self.k21*A2
+            return [eq1, eq2]
+
+        def const_influx(self, interval, A):
+            C1, C2 = A[0], A[1]
+            R_in = (self.F*self.A_gut)/self.tau
+            eq1 = R_in/self.V1 + self.k21*C2- self.k12*C1-self.K*C1
+            eq2 = self.k12*C1 - self.k21*C2
+            return [eq1, eq2]
+
+        def plot(self , flag, t):
+            if flag == "TWO COMPARTMENT-Intravenous Bolus( SINGLE DOSE )":
+                self.A1 = self.A_gut
+                A = solve_ivp(self.solver, (0,25), (self.A1, self.A2), t_eval = t)
+                C1 = A.y[0].flatten()/self.V1
+                C2 = A.y[1].flatten()/self.V2
+                # self.A_gut = 0
+                plt.plot(t, np.log(C1))
+                plt.title("C1", size=9)
+                plt.xlabel("Hours", size=9)
+                plt.ylabel("Concentration(mg/L)", size=9)
+            
+            elif flag == "TWO COMPARTMENT-ORAL( MULTIPLE DOSE )":
+                
+                A = solve_ivp(self.solver_multiple, (0,25), (self.A1, self.A2), t_eval = t)
+                C1 = A.y[0].flatten()/self.V1
+                C2 = A.y[1].flatten()/self.V2
+ 
+                y = [0]*len(t)
+                for j in range(t.shape[0]):
+                    y[j] = self.val_t(t[j], self.tau, self.max_dose, C1 )
+                plt.plot(t, y)
+                plt.title("C1", size=9)
+                plt.xlabel("Hours", size=9)
+                plt.ylabel("Concentration(mg/L)", size=9)
+
+            elif flag == "TWO COMPARTMENT-Intravenous Bolus( CONSTANT RATE INFLUX)":
+                
+                A = solve_ivp(self.const_influx, (0,25), (self.A1, self.A2), t_eval = t)
+                C1 = A.y[0].flatten()
+                C2 = A.y[1].flatten()
+                plt.plot(t, C1)
+                plt.title("C1", size=9)
+                plt.xlabel("Hours", size=9)
+                plt.ylabel("Concentration(mg/L)", size=9)                
+                
+            elif flag == "TWO COMPARTMENT-ORAL( SINGLE DOSE )":
+                A = solve_ivp(self.oral_singleDose, (0,25), (self.A1, self.A2, self.A_gut), t_eval = t)
+                C1 = A.y[0].flatten()/self.V1
+                C2 = A.y[1].flatten()/self.V2
+                # self.A_gut = 0
+                plt.plot(t, C1)
+                plt.title("C1", size=9)
+                plt.xlabel("Hours", size=9)
+                plt.ylabel("Concentration(mg/L)", size=9)
+            plt.show()
+
+        def md_unit(self,n, t, C):
+            # f = 25*(np.exp(-k*(t-n*tau)) - np.exp(-k_a*(t-n*tau)))
+            tau = self.tau
+            f = C[int(t-n*tau)]
+            return f
+
+        def val_t(self,t, tau, max_dose, C):
+            max_term = int(t/tau) if int(t/tau) < self.max_dose else self.max_dose 
+            cumm = self.md_unit(0, t, C)
+            for i in range(1, max_term+1):
+                cumm+=self.md_unit(i, t, C)
+            return cumm
